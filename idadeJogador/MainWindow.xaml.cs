4892f111b69593bb1f10e7483fc774ab871ff882
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-09-27 11:29:20 -0300

    Projeto idade do jogador

---
+++ idadeJogador/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace idadeJogador
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, RoutedEventArgs e)
+        {
+            byte idade;
+
+            //entrada de dados
+            idade = Convert.ToByte(txtIdade.Text);
+
+            //Condicional
+            if (idade >= 12)//Desvio condicional Composto (trata verdadeiro e falso)
+            {
+                //BLOCO VERDADEIRO
+                MessageBox.Show("Você pode jogar!", "Idade do Jogador",
+                                  MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                //BLOCO FALSO
+                MessageBox.Show("Você NÃO pode jogar!", "Idade do Jogador",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
